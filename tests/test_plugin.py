8fa896377bb850eeaf0cc2162e4fa4c4f811c21d
bprinty <bprinty@gmail.com>
2019-11-04 23:23:18 -0600

    Added tests for new records property

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -20,7 +20,7 @@ class TestVersioning(object):
         assert response.json['name'] == items[0].name
         return
 
-    def test_history(self, client):
+    def test_records(self, client):
         # create item and update it
         item = ItemFactory.create(name='versioning 1')
         response = client.put('/items/{}'.format(item.id), json=dict(
@@ -31,13 +31,13 @@ class TestVersioning(object):
 
         # test versioning properties
         item = db.session.query(Item).filter_by(id=item.id).one()
-        assert len(item.history) == 2
+        assert len(item.records) == 2
         assert item.versions[0].name == 'versioning 1'
         assert item.versions[1].name == 'versioning 2'
-        assert item.history[0].json()['name'] == 'versioning 1'
-        assert item.history[0].json()['id'] == item.id
-        assert item.history[1].json()['name'] == 'versioning 2'
-        assert item.history[1].json()['id'] == item.id
+        assert item.records[0].json()['name'] == 'versioning 1'
+        assert item.records[0].json()['id'] == item.id
+        assert item.records[1].json()['name'] == 'versioning 2'
+        assert item.records[1].json()['id'] == item.id
         return
 
     def test_revert(self, client):
@@ -56,10 +56,10 @@ class TestVersioning(object):
 
         # test that multiple versions exist
         item = db.session.query(Item).filter_by(name='revert 2').one()
-        assert len(item.history) == 2
+        assert len(item.records) == 2
 
         # revert one of the versions
-        version = item.versions[0]
+        version = item.records[0]
         version.revert()
         db.session.commit()
         response = client.get('/items/{}'.format(item.id))

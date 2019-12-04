ff306a7573cc3d3936ac5d6c67433f0994af3e33
bprinty <bprinty@gmail.com>
2019-12-03 22:30:13 -0600

    Incremented patch version for new feature

--- flask_continuum/__init__.py
+++ flask_continuum/__init__.py
@@ -5,7 +5,7 @@ __url__ = 'https://github.com/bprinty/Flask-Continuum'
 __info__ = 'Model provenance and versioning via SQLAlchemy-Continuum'
 __author__ = 'Blake Printy'
 __email__ = 'bprinty@gmail.com'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 
 from .mixins import VersioningMixin      ## noqa
